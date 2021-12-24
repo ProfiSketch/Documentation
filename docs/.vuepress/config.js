3c377e38186f64358bbfb1d5e1e6976a2b87ab11
ProfiSketch <it@profisketch.com>
2021-12-24 23:28:52 +0300

    bugfix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,12 +30,15 @@ module.exports = {
 
                 "/ps/profile/",                   // 1.   Личный кабинет пользователя
 
-                children: [
-                    "/ps/profile/registration",       // 1.1. Регистрация
-                    "/ps/profile/create_project",     // 1.2. Создание проекта
-                ]
+                {
+                    children: [
+                        "/ps/profile/registration",       // 1.1. Регистрация
+                        "/ps/profile/create_project",     // 1.2. Создание проекта
+                    ]
+                }
 
-                "/ps/editor2d/",                  // 2.   Создание 2D-схемы
+
+                "/ps/editor2d",                   // 2.   Создание 2D-схемы
                 "/ps/editor2d/shapes_placing",    // 2.1. Размещение фигур
                 "/ps/editor2d/libraries",         // 2.2. Библиотеки условно-графических обозначений
                 "/ps/editor2d/searching",         // 2.3. Поиск фигур
